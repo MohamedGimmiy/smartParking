2901c5032403369a80d86e2998a11adc5c1e182a
MohamedGimmiy <mohamedhussin07@gmail.com>
2019-03-24 06:11:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = 'HomePage'; // lazy loading
+
+  pages: Array<{ title: string, component: any,ico:any }>;
+
+  constructor(private events: Events, 
+              public platform: Platform, 
+              public statusBar: StatusBar,
+              public splashScreen: SplashScreen
+) {
+              
+
+
+    this.events.subscribe('RESPONSE:ERROR', (error: any) => {
+
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
     });
+    this.pages = [
+      { title: 'Home', component: 'HomePage',ico:'ios-home' },
+      { title: 'Settings', component: 'SettingsPage',ico:'ios-settings' }
+    ];
+  }
+  openPage(page) {
+    this.nav.setRoot(page.component);
+
+  }
+  logoutNav() {
+    this.nav.setRoot('HomePage');
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {IonicPage, NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  blutoothUnpairedDevices : any;
+  pairedDevices : any;
+  selectedDevice : any;
+  constructor(public navCtrl: NavController,
+              public blutooth:BluetoothSerial) {
+
+                // check if blutooth is enabled or not
+                // then if not display a message to enable blutooth
+/*                 this.blutooth.isEnabled().then(()=>{
+                  // blutooth is enabled
+                },()=>{
+                  // display message if blutooth is not enabled
+                  // ask user to enable the blutooth
+                  this.blutooth.enable().then(()=>{
+                    console.log("blutooth is requested");
+                  });
+                }); */
+
 
   }
+  ionViewWilEnter(){
+      // paired devices 
+      this.blutooth.list().then(data=>{
+        this.pairedDevices = data;
+      });
+  }
+  //-------------------- scaning for available devices ------------------//
+  scan(){
 
+    this.blutooth.isEnabled().then(()=>{
+      // blutooth is available
+      // get all devices available
+      this.blutooth.discoverUnpaired().then(data=>{
+        this.blutoothUnpairedDevices = data; // unpaird devices
+      });
+
+    },()=>{
+      // blutooth is not available
+      this.blutooth.enable().then(()=>{
+        console.log("blutooth is requested");
+      });
+    });
+
+  }
+  // ----------------- choose a selected device ----------------- //
+  chooseDevice(){
+    this.blutooth.connect(this.selectedDevice.address).subscribe(()=>{
+      alert("conncted succrssfully");
+      console.log("connected successfully");
+    },()=>{
+      alert("connected failed");
+      console.log("connection failed")
+    });
+  }
 }
